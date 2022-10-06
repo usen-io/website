0c23580243e934125e911ff7df2aa54a79ed766d
Anibal Aguila <aa@usen.io>
2022-10-06 23:49:33 +0200

    content: addition

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -145,7 +145,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `KISS |•~| ${new Date().getFullYear()}.`,
       },
       prism: {
         theme: lightCodeTheme,
