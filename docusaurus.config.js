f4f8eff36c806103d6a990c2ce798b5534a4cdfa
Anibal Aguila <aa@usen.io>
2022-10-06 23:50:59 +0200

    content: addition

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,45 +105,45 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
+        //  {
+        //    title: 'Docs',
+        //    items: [
+        //      {
+        //        label: 'Tutorial',
+        //        to: '/docs/intro',
+        //      },
+        //    ],
+        //  },
+        //  {
+        //    title: 'Community',
+        //    items: [
+        //      {
+        //        label: 'Stack Overflow',
+        //        href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //      },
+        //      {
+        //        label: 'Discord',
+        //        href: 'https://discordapp.com/invite/docusaurus',
+        //      },
+        //      {
+        //        label: 'Twitter',
+        //        href: 'https://twitter.com/docusaurus',
+        //      },
+        //    ],
+        //  },
+        //  {
+        //    title: 'More',
+        //    items: [
+        //      {
+        //        label: 'Blog',
+        //        to: '/blog',
+        //      },
+        //      {
+        //        label: 'GitHub',
+        //        href: 'https://github.com/facebook/docusaurus',
+        //      },
+        //    ],
+        //  },
         ],
         copyright: `KISS |•~| ${new Date().getFullYear()}.`,
       },
